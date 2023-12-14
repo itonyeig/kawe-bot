4504266ab59207f8b08ebc9227b5c9e484064218
itonyeig <itonyeig@gmail.com>
2023-12-14 10:11:34 +0100

    Add object for storing user book preference questions

--- utils/recommendation-questions.ts
+++ utils/recommendation-questions.ts
@@ -1,10 +1,10 @@
 // const bookRecommendationQuestions: : Readonly<{q1: string}>= 
 const bookRecommendationQuestions: Readonly<{ q1: string; q2: string; q3: string; q4: string; q5: string; }> = {
-    "q1": "What type of books do you usually enjoy reading? You can mention specific genres like mystery, science fiction, romance, or non-fiction topics that interest you.",
-    "q2": "Could you name a few of your favorite authors or books? This will help me understand your reading preferences better.",
-    "q3": "Are you looking for a book that matches a particular mood or theme? For example, are you in the mood for something uplifting, thought-provoking, or adventurous?",
-    "q4": "What is your main purpose for reading right now? Are you looking to learn something new, escape into a different world, or perhaps you're seeking a book for professional development?",
-    "q5": "Do you have a preference for the length or complexity of the books you read? For example, do you prefer quick, easy reads, or are you okay with longer, more detailed narratives?"
-  }
-  
+  "q1": "What type of books do you usually enjoy reading? You can mention specific genres like mystery, science fiction, romance, or non-fiction topics that interest you.",
+  "q2": "Could you name a few of your favorite authors or books? This will help me understand your reading preferences better.",
+  "q3": "Are you looking for a book that matches a particular mood or theme? For example, are you in the mood for something uplifting, thought-provoking, or adventurous?",
+  "q4": "What is your main purpose for reading right now? Are you looking to learn something new, escape into a different world, or perhaps you're seeking a book for professional development?",
+  "q5": "Do you have a preference for the length or complexity of the books you read? For example, do you prefer quick, easy reads, or are you okay with longer, more detailed narratives?"
+}
+
   
